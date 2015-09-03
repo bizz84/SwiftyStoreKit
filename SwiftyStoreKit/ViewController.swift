9e80861d7e538113dff1d1a984adbc4a7a6a98cd
Andrea Bizzotto <bizz84@gmail.com>
2015-09-03 11:28:05 +0100

    Initial Commit

---
+++ SwiftyStoreKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyStoreKit
+//
+//  Created by Andrea Bizzotto on 03/09/2015.
+//  Copyright © 2015 musevisions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

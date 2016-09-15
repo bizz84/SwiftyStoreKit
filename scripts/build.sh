f468394407a770aa6ece747ed09cdd7c17358885
Andrea Bizzotto <bizz84@gmail.com>
2016-09-15 22:31:50 +0100

    Add build scripts for travis CI integration

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit
+xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKitOSX
+xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKitTV

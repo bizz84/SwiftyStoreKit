74c700e7cf6b7f33fc2c3e0a75096f2e563890b1
Andrea Bizzotto <bizz84@gmail.com>
2017-01-21 15:03:16 +0000

    Add unit tests target to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,5 @@
 xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_iOS
 xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_macOS
 xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_tvOS
+
+xcodebuild test -project SwiftyStoreKit.xcodeproj -scheme SwiftyStoreKitTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3'

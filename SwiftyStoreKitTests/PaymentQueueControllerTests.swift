c76269c4c22e4a35b5ec3d66a1a50907186371b6
Andrea Bizzotto <bizz84@gmail.com>
2017-02-21 21:59:37 +0000

    Removed function_body_length from .swiftlint.yml and disabled it in the only test file that breaks the default 40 line limit

--- SwiftyStoreKitTests/PaymentQueueControllerTests.swift
+++ SwiftyStoreKitTests/PaymentQueueControllerTests.swift
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// swiftlint:disable function_body_length
+
 import XCTest
 import StoreKit
 @testable import SwiftyStoreKit

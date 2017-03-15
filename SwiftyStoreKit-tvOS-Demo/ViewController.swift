6dd9920309424e92b48c3ff14dce3d60ca8cac67
Andrea Bizzotto <bizz84@gmail.com>
2017-03-15 22:23:41 +0000

    Added tvOS demo target

---
+++ SwiftyStoreKit-tvOS-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyStoreKit-tvOS-Demo
+//
+//  Created by Andrea Bizzotto on 15/03/2017.
+//  Copyright © 2017 musevisions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

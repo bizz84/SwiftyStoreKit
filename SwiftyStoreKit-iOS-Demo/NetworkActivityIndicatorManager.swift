07aa26c558962bca9c056ccc2ff2cc0fc063db57
Andrea Bizzotto <bizz84@gmail.com>
2017-03-16 21:28:49 +0000

    Share iOS ViewController on tvOS

--- SwiftyStoreKit-iOS-Demo/NetworkActivityIndicatorManager.swift
+++ SwiftyStoreKit-iOS-Demo/NetworkActivityIndicatorManager.swift
@@ -30,18 +30,22 @@ class NetworkActivityIndicatorManager: NSObject {
 
     class func networkOperationStarted() {
 
+        #if os(iOS)
         if loadingCount == 0 {
             UIApplication.shared.isNetworkActivityIndicatorVisible = true
         }
         loadingCount += 1
+        #endif
     }
 
     class func networkOperationFinished() {
+        #if os(iOS)
         if loadingCount > 0 {
             loadingCount -= 1
         }
         if loadingCount == 0 {
             UIApplication.shared.isNetworkActivityIndicatorVisible = false
         }
+        #endif
     }
 }

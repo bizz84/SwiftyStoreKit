4d8e144a99359a02429a5b479d9204f318ea0122
Andrea Bizzotto <bizz84@gmail.com>
2017-05-18 17:01:17 +0100

    Pipe build output with xcpretty

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_iOS
-xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_macOS
-xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_tvOS
+xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_iOS | tee xcodebuild.log | xcpretty
+xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_macOS | tee xcodebuild.log | xcpretty
+xcodebuild -project SwiftyStoreKit.xcodeproj -target SwiftyStoreKit_tvOS | tee xcodebuild.log | xcpretty
 
-xcodebuild test -project SwiftyStoreKit.xcodeproj -scheme SwiftyStoreKitTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3'
+xcodebuild test -project SwiftyStoreKit.xcodeproj -scheme SwiftyStoreKitTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.3' | tee xcodebuild.log | xcpretty

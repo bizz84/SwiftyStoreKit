3edfa0d67163796216fad602ca23c15b9b12a65d
Andrea Bizzotto <bizz84@gmail.com>
2017-08-21 09:55:14 +0100

    Update install swiftlint script to version 0.21.0

--- scripts/install_swiftlint.sh
+++ scripts/install_swiftlint.sh
@@ -7,7 +7,7 @@
 set -e
 
 SWIFTLINT_PKG_PATH="/tmp/SwiftLint.pkg"
-SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.18.1/SwiftLint.pkg"
+SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.21.0/SwiftLint.pkg"
 
 wget --output-document=$SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
 

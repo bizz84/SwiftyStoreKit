88f266f60413a0333e1cba8e6cdc56786866b702
Mario Hahn <mario_hahn@me.com>
2020-06-25 14:03:08 +0200

    fix path

--- Package.swift
+++ Package.swift
@@ -24,9 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyStoreKit",
-            dependencies: []),
+            dependencies: [],
+            path: "SwiftyStoreKit"),
         .testTarget(
             name: "SwiftyStoreKitTests",
-            dependencies: ["SwiftyStoreKit"]),
+            dependencies: ["SwiftyStoreKit"],
+            path: "SwiftyStoreKitTests"),
     ]
 )

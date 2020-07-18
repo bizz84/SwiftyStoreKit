d3bee96cd24720692e6dcd15f12002868524fb18
Stefanos Zachariadis <stefanos@zachariadis.net>
2020-07-18 10:47:16 +0100

    set minimum target versions. Otherwise cannot deploy using swiftpm to watchos

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SwiftyStoreKit",
-    // platforms: [.iOS("8.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
+    platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("6.2")],
     products: [
         .library(name: "SwiftyStoreKit", targets: ["SwiftyStoreKit"])
     ],

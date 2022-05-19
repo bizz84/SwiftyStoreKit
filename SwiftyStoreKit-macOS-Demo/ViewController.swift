c98e90b518eef4e0728126115ef4973e33c110c9
Ivan <6350992+bivant@users.noreply.github.com>
2022-05-19 22:58:00 +0300

    Fix demo compilation error after b3f2601196a87569d463283c3a582ddcc7fccf0b - Merge pull request #652 from azouts/master Communicate deferred transactions to the app

--- SwiftyStoreKit-macOS-Demo/ViewController.swift
+++ SwiftyStoreKit-macOS-Demo/ViewController.swift
@@ -193,6 +193,8 @@ extension ViewController {
         case .success(let purchase):
             print("Purchase Success: \(purchase.productId)")
             return alertWithTitle("Thank You", message: "Purchase completed")
+        case .deferred(purchase: _):
+            return alertWithTitle("Purchase deferred", message: "The purchase deferred")
         case .error(let error):
             print("Purchase Failed: \(error)")
             switch error.code {
